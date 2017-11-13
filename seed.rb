5665a8c38a0b66e70f4060bd7245defd8e287bf8
Denis <denisosa911@gmail.com>
2017-11-13 11:05:19 +0200

    The map method was used to populate the array.

--- seed.rb
+++ seed.rb
@@ -2,38 +2,15 @@ require 'faker'
 require_relative 'collage'
 require_relative 'student'
 @student = Student.new
-students1 = []
-i = 0
-loop do
-  @student = Faker::Name.name
-  students1 << @student
-  i += 1
-  break if i == 10
-end
-students2 = []
-i = 0
-loop do
-  @student = Faker::Name.name
-  students2 << @student
-  i += 1
-  break if i == 10
-end
-students3 = []
-i = 0
-loop do
-  @student = Faker::Name.name
-  students3 << @student
-  i += 1
-  break if i == 10
-end
-students4 = []
-i = 0
-loop do
-  @student = Faker::Name.name
-  students4 << @student
-  i += 1
-  break if i == 10
-end
+students1 = Array.new(10)
+students2 = Array.new(10)
+students3 = Array.new(10)
+students4 = Array.new(10)
+@student = Faker::Name.name
+students1 = students1.map { |_| @student }
+students2 = students2.map { |_| @student }
+students3 = students3.map { |_| @student }
+students4 = students4.map { |_| @student }
 
 @collage = Collage.new('collage')
 @collage.software_fac.students = students1
